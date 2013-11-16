ecf32b6beda6d36c983e16b9ee0be8c842410dbe
Christian Noon <christian.noon@gmail.com>
2013-11-16 14:21:53 -0600

    Added an inclusive header.

---
+++ Parse/Parse.h
@@ -0,0 +1,22 @@
+//
+//  Parse.h
+//  Parse
+//
+//  Created by Christian Noon on 11/16/13.
+//  Copyright (c) 2013 Christian Noon. All rights reserved.
+//
+
+#ifndef PARSE_PARSE_H
+#define PARSE_PARSE_H
+
+#include "PFACL.h"
+#include "PFDateTime.h"
+#include "PFError.h"
+#include "PFFile.h"
+#include "PFManager.h"
+#include "PFObject.h"
+#include "PFSerializable.h"
+#include "PFTypedefs.h"
+#include "PFUser.h"
+
+#endif	// End of PARSE_PARSE_H
